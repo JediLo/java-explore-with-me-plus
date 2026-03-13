899657a047cde715c454deeb06a3c250310345a3
Novikov Aleksei <3900676@gmail.com>
2026-03-13 22:24:46 +0300

    added main

--- stats/stats-server/src/main/java/ru/practicum/StatsServer.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsServer.java
@@ -1,12 +1,11 @@
-package ru.practicum.service;
+package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationExploreWithMeStats {
+public class StatsServer {
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationExploreWithMeStats.class, args);
-
+        SpringApplication.run(StatsServer.class, args);
     }
 }
