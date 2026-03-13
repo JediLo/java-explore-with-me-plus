8dda415f976d85dce3559d12404817a4ddb0f450
JediLon <r.lipatenkov@gmail.com>
2026-03-13 05:03:38 +0200

    update maven modules and dependencies

--- main-service/src/main/java/ru/practicum/explorewithme/ApplicationExploreWithMe.java
+++ main-service/src/main/java/ru/practicum/explorewithme/ApplicationExploreWithMe.java
@@ -3,6 +3,7 @@ package ru.practicum.explorewithme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApplicationExploreWithMe {
     public static void main(String[] args) {
