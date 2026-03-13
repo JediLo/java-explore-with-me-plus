e270f13abce09ab0ff3b054e3627e12a0f5fe8a7
desmoo00 <dimckalegend2@yandex.com>
2026-03-13 12:37:23 -0500

    Добавлен функционал client

--- stats/stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -1,21 +1,54 @@
 package ru.practicum;
 
-
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.practicum.dto.EndPointHitDto;
 import ru.practicum.model.ViewStats;
 
-import java.util.Collection;
+import java.util.List;
 
+@Service
 public class StatsClient {
 
-    // Rest Client
+    private final RestTemplate restTemplate;
 
-    public ViewStats getStats(Collection<ViewStats> viewStats) {
-
-        return new ViewStats();
+    @Autowired
+    public StatsClient(@Value("${stats-server.url:http://stats-server:9090}") String serverUrl, RestTemplateBuilder builder) {
+        this.restTemplate = builder
+                .rootUri(serverUrl)
+                .build();
     }
 
-    public void hit(ViewStats viewStats) {
+    public void saveHit(EndPointHitDto hitDto) {
+        restTemplate.postForEntity("/hit", hitDto, Void.class);
+    }
 
+    public List<ViewStats> getStats(String start, String end, List<String> uris, Boolean unique) {
+        UriComponentsBuilder builder = UriComponentsBuilder.fromPath("/stats")
+                .queryParam("start", start)
+                .queryParam("end", end);
+
+        if (uris != null && !uris.isEmpty()) {
+            builder.queryParam("uris", String.join(",", uris));
+        }
+        if (unique != null) {
+            builder.queryParam("unique", unique);
+        }
+
+        ResponseEntity<List<ViewStats>> response = restTemplate.exchange(
+                builder.build().toUriString(),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+
+        return response.getBody();
     }
 }
