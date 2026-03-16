7a6f94c51f8180a6e8d4dc751e7cec91a5ef8532
JediLon <r.lipatenkov@gmail.com>
2026-03-16 21:38:41 +0200

    Add validate to EndPointDto, remove mappers

--- stats/stats-server/src/main/java/ru/practicum/StatsService.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsService.java
@@ -2,6 +2,7 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.practicum.dto.EndPointHitDto;
 
 @SpringBootApplication
 public class StatsService {
