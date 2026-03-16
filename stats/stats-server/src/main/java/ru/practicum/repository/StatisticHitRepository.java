4e813ab71ffbaa3190930a2008bc94f2e609fcf4
Novikov Aleksei <3900676@gmail.com>
2026-03-16 19:45:12 +0300

    fix checkstyle error

--- stats/stats-server/src/main/java/ru/practicum/repository/StatisticHitRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/StatisticHitRepository.java
@@ -19,17 +19,17 @@ public interface StatisticHitRepository extends JpaRepository<StatisticHit, Long
             order by count(h.id) desc
             """)
     List<ViewStatsDto> getStats(@Param("start") LocalDateTime start,
-                               @Param("end") LocalDateTime end);
+                                @Param("end") LocalDateTime end);
 
     @Query("""
             select new ru.practicum.dto.ViewStatsDto(h.app, h.uri, count(distinct h.ip))
             from StatisticHit h
             where h.hitTime between :start and :end
             group by h.app, h.uri
-            order by count(distinct h.ip) desc        
+            order by count(distinct h.ip) desc
             """)
     List<ViewStatsDto> getUniqueStats(@Param("start") LocalDateTime start,
-                                     @Param("end") LocalDateTime end);
+                                      @Param("end") LocalDateTime end);
 
     @Query("""
             select new ru.practicum.dto.ViewStatsDto(h.app, h.uri, count(h.id))
@@ -40,8 +40,8 @@ public interface StatisticHitRepository extends JpaRepository<StatisticHit, Long
             order by count(h.id) desc
             """)
     List<ViewStatsDto> getStatsByUris(@Param("start") LocalDateTime start,
-                                     @Param("end") LocalDateTime end,
-                                     @Param("uris") List<String> uris);
+                                      @Param("end") LocalDateTime end,
+                                      @Param("uris") List<String> uris);
 
     @Query("""
             select new ru.practicum.dto.ViewStatsDto(h.app, h.uri, count(distinct h.ip))
@@ -52,6 +52,6 @@ public interface StatisticHitRepository extends JpaRepository<StatisticHit, Long
             order by count(distinct h.ip) desc
             """)
     List<ViewStatsDto> getUniqueStatsByUris(@Param("start") LocalDateTime start,
-                                           @Param("end") LocalDateTime end,
-                                           @Param("uris") List<String> uris);
+                                            @Param("end") LocalDateTime end,
+                                            @Param("uris") List<String> uris);
 }
