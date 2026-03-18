a3bba1631ddf9b7f00a4e8794bd107f08d12451f
Novikov Aleksei <3900676@gmail.com>
2026-03-18 22:53:22 +0300

    added Exceptions

--- main-service/src/main/java/ru/practicum/explorewithme/exceptions/dto/ApiError.java
+++ main-service/src/main/java/ru/practicum/explorewithme/exceptions/dto/ApiError.java
@@ -14,15 +14,10 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApiError {
-
     private List<String> errors;
-
     private String message;
-
     private String reason;
-
     private String status;
-
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
