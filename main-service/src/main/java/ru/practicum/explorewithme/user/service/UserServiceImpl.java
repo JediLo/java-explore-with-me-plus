b16f64492a6ee1fbb973221a40db40c7fecb3557
Novikov Aleksei <3900676@gmail.com>
2026-03-19 09:56:44 +0300

    added rest /admin/users

---
+++ main-service/src/main/java/ru/practicum/explorewithme/user/service/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package ru.practicum.explorewithme.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.explorewithme.user.dto.NewUserRequest;
+import ru.practicum.explorewithme.user.dto.UserDto;
+import ru.practicum.explorewithme.user.mapper.UserMapper;
+import ru.practicum.explorewithme.user.model.User;
+import ru.practicum.explorewithme.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDto registerUser(NewUserRequest request) {
+        User user = UserMapper.toUser(request);
+        User saved = userRepository.save(user);
+        return UserMapper.toUserDto(saved);
+    }
+}
