7586fe9b1b7089430a85e1c625c7d44e7612a85e
Novikov Aleksei <3900676@gmail.com>
2026-03-19 16:42:08 +0300

    added rest /admin/categories

---
+++ main-service/src/main/java/ru/practicum/explorewithme/category/controller/AdminCategoryController.java
@@ -0,0 +1,30 @@
+package ru.practicum.explorewithme.category.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.explorewithme.category.dto.CategoryDto;
+import ru.practicum.explorewithme.category.dto.NewCategoryDto;
+import ru.practicum.explorewithme.category.service.CategoryService;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin/categories")
+@RequiredArgsConstructor
+public class AdminCategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDto addCategory(@RequestBody @Valid NewCategoryDto dto) {
+        log.debug("POST /admin/categories: name='{}'", dto.getName());
+        return categoryService.addCategory(dto);
+    }
+}
---
+++ main-service/src/main/java/ru/practicum/explorewithme/category/service/CategoryServiceImpl.java
@@ -0,0 +1,26 @@
+package ru.practicum.explorewithme.category.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.explorewithme.category.dto.CategoryDto;
+import ru.practicum.explorewithme.category.dto.NewCategoryDto;
+import ru.practicum.explorewithme.category.mapper.CategoryMapper;
+import ru.practicum.explorewithme.category.model.Category;
+import ru.practicum.explorewithme.category.repository.CategoryRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    @Transactional
+    public CategoryDto addCategory(NewCategoryDto dto) {
+        Category category = CategoryMapper.toCategory(dto);
+        Category saved = categoryRepository.save(category);
+        return CategoryMapper.toCategoryDto(saved);
+    }
+}
