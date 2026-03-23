3aaac7353c8313312e7f53ebf0e26d75fac5d224
Novikov Aleksei <3900676@gmail.com>
2026-03-23 18:26:32 +0300

    fix service

--- main-service/src/main/java/ru/practicum/explorewithme/event/service/EventService.java
+++ main-service/src/main/java/ru/practicum/explorewithme/event/service/EventService.java
@@ -1,9 +1,13 @@
 package ru.practicum.explorewithme.event.service;
 
 import ru.practicum.explorewithme.event.dto.EventFullDto;
+import ru.practicum.explorewithme.event.dto.EventShortDto;
 import ru.practicum.explorewithme.event.dto.NewEventDto;
 
+import java.util.List;
 
 public interface EventService {
     EventFullDto addEvent(long userId, NewEventDto dto);
+
+    List<EventShortDto> getEvents(long userId, int from, int size);
 }
