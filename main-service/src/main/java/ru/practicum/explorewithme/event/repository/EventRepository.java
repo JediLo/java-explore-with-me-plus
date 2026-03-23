93aa8dab50fe69f55d47d462010f57bc898c9908
Novikov Aleksei <3900676@gmail.com>
2026-03-23 18:25:53 +0300

    added repository

--- main-service/src/main/java/ru/practicum/explorewithme/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/explorewithme/event/repository/EventRepository.java
@@ -1,7 +1,13 @@
 package ru.practicum.explorewithme.event.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph;
 import ru.practicum.explorewithme.event.model.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @EntityGraph(attributePaths = {"initiator", "category"})
+    Page<Event> findAllByInitiatorId(Long initiatorId, Pageable pageable);
 }
